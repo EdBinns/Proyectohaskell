686dbf617369488fbc7cc3c168555d6fdaf32739
EdBinns <eduardo.binns.sanchez@gmail.com>
2020-10-24 23:47:34 -0600

    Formularios completos

--- proyecto.hs
+++ proyecto.hs
@@ -17,37 +17,54 @@ input  = do
 
 showMessage :: String -> IO()
 showMessage  x = print  x
----Agregar Encuestas
-generateQuestions :: Int -> [[[String]]] -> IO [[[String]]]
-generateQuestions x y = do 
+addFormsNames ::Int -> String -> [[[String]]]-> IO [[[String]]]
+addFormsNames x y z =  do
+     if (x) /= 0
+        then do
+            let message = "Ingrese el nombre de la encuesta"
+            showMessage message
+            newName <- input
+            print "Seguidamente va a agregar las preguntas"
+            let forms = generateQuestions 1 newName [] 
+            formsLists <- (forms)
+            let completeForms = z ++ formsLists
+            print("Quiere agregar otra Encuesta? 1 para si y 0 para no")
+            varInput <- input
+            let varCondicion = read varInput :: Int
+            addFormsNames varCondicion y completeForms
+        else return  z
+---Agregar preguntas
+generateQuestions :: Int -> String ->[[[String]]] -> IO [[[String]]]
+generateQuestions x y z= do 
    if (x) /= 0
         then do
+            showMessage "Ingrese 1 para que sea de tipo escalar o 2 para que sea de selección unica"
+            questionType <- input
             showMessage "Ingrese la pregunta o titulo que desee"
             newQuestion <- input
-            let newQuestionList = addForms newQuestion []
-            showMessage "Ingrese 1 para que sea de tipo escalar o 2 para que sea de selección unica"
-            questionType <- input 
+            let newQuestionList = addQuestions newQuestion []
+            let questionAndNameFormList = [y] ++ newQuestionList
             let varType = read questionType :: Int
             if(varType) == 2
                 then do
                        let questionsIO =  addUniqueSelection 1 newQuestion []
                        unique <- (questionsIO)
-                       let completeForm = addCompleteForm newQuestionList unique
-                       let concatList = y ++ completeForm
+                       let completeForm = addCompleteForm questionAndNameFormList unique
+                       let concatList = z ++ completeForm
                        print("Quiere agregar otra pregunta? 1 para si y 0 para no")
                        varInput <- input
                        let varCondicion = read varInput :: Int
-                       generateQuestions varCondicion concatList
+                       generateQuestions varCondicion y concatList
                 else  do
                        let questionsIO =  addScaleQuestions 0 newQuestion []
                        scaleAnsers <- (questionsIO)
-                       let completeForm = addCompleteForm newQuestionList scaleAnsers
-                       let concatList = y ++ completeForm
+                       let completeForm = addCompleteForm questionAndNameFormList scaleAnsers
+                       let concatList = z ++ completeForm
                        print("Quiere agregar otra pregunta? 1 para si y 0 para no")
                        varInput <- input
                        let varCondicion = read varInput :: Int
-                       generateQuestions varCondicion concatList
-        else  return y
+                       generateQuestions varCondicion y concatList
+        else  return z
 
 ---Agregar respuestas para  preguntas  de selección unica 
 addUniqueSelection ::Int -> String -> [String]-> IO [String]
@@ -79,8 +96,7 @@ addScaleQuestions x y z = do
 
 main :: IO ()
 main = do
-
-    let forms = generateQuestions 1 []
+    let forms = addFormsNames 1 " " []
     x <- (forms)
     print x
 
